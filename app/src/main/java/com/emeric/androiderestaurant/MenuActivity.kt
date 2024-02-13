b305e6dfd5c0c8699b0590bd49a9ece94a11ee96
ejanowski <janowski.emeric@gmail.com>
2024-02-13 15:40:03 +0100

    oncick

--- app/src/main/java/com/emeric/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/com/emeric/androiderestaurant/MenuActivity.kt
@@ -5,10 +5,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -19,7 +31,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -67,7 +87,8 @@ fun MenuView(type: DishType) {
     val category = remember {
         mutableStateOf<Category?>(null)
     }
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         Text(type.title())
         LazyColumn {
             category.value?.let {
@@ -81,8 +102,38 @@ fun MenuView(type: DishType) {
 }
 
 @Composable fun dishRow(dish: Dish) {
-    Card {
-        Text(dish.name)
+    Card(border =  BorderStroke(1.dp, Color.Black),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable {
+                Log.d("click", "click")
+            }
+    ) {
+        Row(Modifier.padding(8.dp)) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(dish.images.first())
+                    .build(),
+                null,
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                error = painterResource(R.drawable.ic_launcher_foreground),
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .width(80.dp)
+                    .height(80.dp)
+                    .clip(RoundedCornerShape(10))
+                    .padding(8.dp)
+            )
+            Text(dish.name,
+                Modifier
+                    .align(alignment = Alignment.CenterVertically)
+                    .padding(8.dp)
+            )
+            Spacer(Modifier.weight(1f))
+            Text("${dish.prices.first().price} €",
+                Modifier.align(alignment = Alignment.CenterVertically))
+        }
     }
 }
 
